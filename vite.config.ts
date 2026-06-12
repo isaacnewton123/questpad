b1ffa4a4c45111076c320ac125ce2cd29c5098b7
isaac newton <hanifmaulana1997@gmail.com>
2026-06-12 11:07:28 +0700

    firs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import checker from 'vite-plugin-checker'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     checker({
       typescript: {
         tsconfigPath: './tsconfig.app.json',
